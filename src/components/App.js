a687b3de296e47ad6008758cdc4a7e97df58130c
Karol <karol.selak@gmail.com>
2017-02-08 00:06:23 +0100

    routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "../stylesheets/main.scss";
 
-// app component
 export default class App extends React.Component {
-  // render
   render() {
-    return (
-      <div className="container">
-        {this.props.children}
-      </div>
-    );
+    return <div className="container">
+      {this.props.main}
+      {this.props.modal}
+    </div>;
   }
 }
--- src/router.js
+++ src/router.js
@@ -2,18 +2,20 @@ import React from "react";
 import { Router, Route, IndexRoute } from "react-router";
 import { history } from "./store.js";
 import App from "./components/App";
-import Home from "./components/Home";
+import Profile from "./components/Profile";
+import Question from "./components/Question";
+import QuestionList from "./components/QuestionList";
 import NotFound from "./components/NotFound";
 
-// build the router
 const router = (
   <Router onUpdate={() => window.scrollTo(0, 0)} history={history}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home}/>
+    <Route component={App}>
+      <Route path="/" components={{main: QuestionList, modal: Profile}} />
+      <Route path="/main/:query/:page/profile/:pid" components={{main: QuestionList, modal: Profile}} />
+      <Route path="/question/:qid/:page/profile/:pid" components={{main: Question, modal: Profile}} />
       <Route path="*" component={NotFound}/>
     </Route>
   </Router>
 );
 
-// export
 export { router };
